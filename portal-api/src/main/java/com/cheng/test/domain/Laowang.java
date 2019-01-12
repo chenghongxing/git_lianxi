10b8491691c80f539ba4b8bcf0b6220ddcd98018
cheng <17853668483@163.com>
2019-01-12 16:44:58 +0800

    Revert "lllll"
    This reverts commit 7a50926

---
+++ portal-api/src/main/java/com/cheng/test/domain/Laowang.java
@@ -0,0 +1,22 @@
+package com.cheng.test.domain;
+
+public class Laowang {
+    private String anme;
+    private String sta;
+
+    public String getAnme() {
+        return anme;
+    }
+
+    public void setAnme(String anme) {
+        this.anme = anme;
+    }
+
+    public String getSta() {
+        return sta;
+    }
+
+    public void setSta(String sta) {
+        this.sta = sta;
+    }
+}
